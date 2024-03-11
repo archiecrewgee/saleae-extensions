ae7f6893b000a5dfa41524b889c223f0580f8e2f
archiecrewgee <archiecg@hotmail.co.nz>
2024-03-11 14:04:42 +0100

    minimum viable integration with salae for analog measument working

--- gradient_analyser/gradient_analyser.py
+++ gradient_analyser/gradient_analyser.py
@@ -1,10 +1,10 @@
 # import math
-# import numpy
+import numpy as np
 
 from saleae.range_measurements import AnalogMeasurer
 
 
-class MyAnalogMeasurer(AnalogMeasurer):
+class GradientAnalyser(AnalogMeasurer):
     supported_measurements = []
 
     # Initialize your measurement extension here
@@ -12,16 +12,28 @@ class MyAnalogMeasurer(AnalogMeasurer):
     def __init__(self, requested_measurements):
         super().__init__(requested_measurements)
 
+        # self.average = 0
+        # self.window = 0
+        # self.peak = 0
+        # self.trough = 0
+
+
+
     # This method will be called one or more times per measurement with batches of data
     # data has the following interface
     #   * Iterate over to get Voltage values, one per sample
     #   * `data.samples` is a numpy array of float32 voltages, one for each sample
     #   * `data.sample_count` is the number of samples (same value as `len(data.samples)` but more efficient if you don't need a numpy array)
     def process_data(self, data):
+
         pass
 
     # This method is called after all the relevant data has been passed to `process_data`
     # It returns a dictionary of the request_measurements values
     def measure(self):
-        values = {}
+        values = {
+            "average" : 0,
+            "peak" :  0,
+            "trough" :  0,
+        }
         return values
